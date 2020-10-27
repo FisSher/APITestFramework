7b29d8d96dd47f22153a3ee19766863b64db2523
Facundo Paolini <39218134+FisSher@users.noreply.github.com>
2020-10-27 17:01:33 -0300

    First commit

---
+++ src/test/java/APITestFramework/StepDefinitions.java
@@ -0,0 +1,34 @@
+package APITestFramework;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Nope";
+    }
+}
+
+@Test
+public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        today = "Sunday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        Assert.assertEquals(expectedAnswer, actualAnswer);
+    }
+}
